f8de396a785ce5b3772cfd417f383010942257a3
Carlos-SSH <carlos.anezeh@me.com>
2020-12-26 13:38:37 -0600

    Created view for form and added validations.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, only: [:new, :create] do
+    resources :users, only: [:new, :create]
+  end
+
+  get 'users/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
